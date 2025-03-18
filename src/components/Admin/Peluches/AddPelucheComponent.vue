7dda2f4096f74e3619f113b88e156616140f7408
akia <ch.royer15@gmail.com>
2025-03-19 00:24:15 +0100

    peluches

--- src/components/Admin/Peluches/AddPelucheComponent.vue
+++ src/components/Admin/Peluches/AddPelucheComponent.vue
@@ -35,55 +35,61 @@
         />
       </div>
 
-      <div class="mb-1 flex column">
-        <InputText id="uploadPresentationImage"
-                   hidden
-                   type="file"
-                   @change="onFileSelect($event)">
-        </InputText>
 
-        <InputGroup class="w-100 mt-2">
-          <InputText placeholder="Selectionner une image"
-                     class="padding-10px width-file-input"
-                     @click="activeInput($event)"
-                     readonly
-                     v-model="presentationImage"/>
-          <Button icon="pi pi-upload" class="background-dark" @click="activeInput($event)"/>
-        </InputGroup>
-
-        <div v-if="thumbnailPresention" class="mt-2 flex width-100 justify-space-between align-items-center">
-          <img :src="thumbnailPresention"
-               class="preview mb-4"
-               alt="image du produit">
-        </div>
-
-        <div v-for="(index) in numberImage" :key="index">
-          <InputText :id="`uploadImages-${index}`"
+      <div class="flex">
+        <div class="mb-1 flex column">
+          <label class="font-size-0_8em italic">Image de présentation</label>
+          <InputText id="uploadPresentationImage"
                      hidden
                      type="file"
-                     @change="onFileSelect($event, index)"/>
-          <InputGroup class="w-100 mt-2">
-            <InputText placeholder="Sélectionner une image"
+                     @change="onFileSelect($event, 'uploadPresentationImage')">
+          </InputText>
+
+          <InputGroup class="w-100">
+            <InputText placeholder="Selectionner une image"
                        class="padding-10px width-file-input"
-                       v-model="imagesName[index - 1]"
-                       @click="activeInput($event, index)"
-                       readonly/>
-            <Button icon="pi pi-upload" class="background-dark" @click="activeInput($event, index)"/>
+                       @click="activeInput($event, 'uploadPresentationImage')"
+                       readonly
+                       v-model="presentationImage"/>
+            <Button icon="pi pi-upload" class="background-dark"
+                    @click="activeInput($event, 'uploadPresentationImage')"/>
           </InputGroup>
 
-          <div v-if="thumbnailsImages[index - 1]" class="mt-2 flex width-100 justify-space-between align-items-center">
-            <img :src="thumbnailsImages[index - 1]"
+          <div v-if="thumbnailPresention" class="mt-2 flex width-100 justify-space-between align-items-center">
+            <img :src="thumbnailPresention"
                  class="preview mb-4"
-                 alt="image du produit"/>
+                 alt="image du produit">
           </div>
         </div>
-      </div>
 
-      <div>
-        <Button label="Ajouter une image"
-                icon="pi pi-plus"
-                @click="numberImage +=1 "
-        ></Button>
+        <div v-if="presentationImage" class="ml-2">
+          <label class="font-size-0_8em italic">Autres images</label>
+          <div class="flex">
+            <div>
+              <InputText :id="`uploadImages`"
+                         hidden
+                         type="file"
+                         @change="onFileSelect($event, 'uploadImages')"/>
+
+              <div v-if="thumbnailsImages.length>0"
+                   class="mt-2 flex width-100 justify-space-between align-items-center">
+                <div v-for="(image, index) in thumbnailsImages" :key="index">
+                  <img v-if="image" :src="image as string"
+                       class="preview mb-4"
+                       alt="image du produit"/>
+                  <p class="pi pi-times" @click="deleteFile(image, index)"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Button label="Ajouter une image"
+                    v-if="thumbnailPresention"
+                    icon="pi pi-plus"
+                    @click="activeInput($event, 'uploadImages')"
+            ></Button>
+          </div>
+        </div>
       </div>
 
       <Button type="submit" severity="secondary" label="Submit" @click="onFormSubmit"/>
@@ -108,68 +114,66 @@ const imagesFiles = ref<File[]>([]);
 const thumbnailsImages = ref<(string | ArrayBuffer)[]>([]);
 const numberImage = ref<number>(0);
 
-const activeInput = (event: Event, n?: number) => {
-  const input = n ? `uploadImages-${n}` : 'uploadPresentationImage';
-  const buttonUpload = document.getElementById(input);
+
+const deleteFile = (image: any, index:number) => {
+  const deleteImage = imagesFiles.value[index]
+  imagesFiles.value = imagesFiles.value.filter((element)=> element !== deleteImage)
+  thumbnailsImages.value = thumbnailsImages.value.filter((element) => element !== image);console.log(imagesFiles.value)
+};
+
+const activeInput = (event: Event, selectInput: 'uploadImages' | 'uploadPresentationImage') => {
+  const buttonUpload = document.getElementById(selectInput);
   if (buttonUpload) {
     buttonUpload.click();
   }
 };
 
-const onFileSelect = async (event: Event, n?: number) => {
+const onFileSelect = async (event: Event, selectFile: 'uploadImages' | 'uploadPresentationImage') => {
   const target = event.target as HTMLInputElement;
   if (!target.files || target.files.length === 0) return;
 
-  if (n) {
-    imagesName.value[n - 1] = target.files[0].name;
-    imagesFiles.value[n - 1] = target.files[0];
-    createThumbnail(n);
+  if (selectFile === 'uploadImages') {
+    imagesName.value.push(target.files[0].name);
+    imagesFiles.value.push(target.files[0]);
+    createThumbnail(target.files[0], 'uploadImages');
   } else {
     presentationImage.value = target.files[0].name;
     filePresentation.value = target.files[0];
-    createThumbnail();
+    createThumbnail(target.files[0], 'uploadPresentationImage');
   }
 };
 
-const createThumbnail = (n?: number) => {
+const createThumbnail = (file: File, selectGroup: 'uploadImages' | 'uploadPresentationImage') => {
   const reader = new FileReader();
 
-  if (n) {
-    if (imagesFiles.value[n - 1]) {
-      reader.onload = (e) => {
-        if (e.target && e.target.result) {
-          thumbnailsImages.value[n - 1] = e.target.result;
-        }
-      };
-      reader.readAsDataURL(imagesFiles.value[n - 1]);
+  reader.onload = (e) => {
+    if (e.target && e.target.result) {
+      if (selectGroup === 'uploadPresentationImage') {
+        thumbnailPresention.value = e.target.result as string;
+      } else {
+        thumbnailsImages.value.push(e.target.result);
+      }
     }
-  } else {
-    if (filePresentation.value) {
-      reader.onload = (e) => {
-        if (e.target && e.target.result) {
-          thumbnailPresention.value = e.target.result as string;
-        }
-      };
-      reader.readAsDataURL(filePresentation.value);
-    }
-  }
+  };
+  reader.readAsDataURL(file);
 };
+
 const onFormSubmit = async (e: any) => {
   e.preventDefault();
-  const allImage = [... imagesFiles.value, filePresentation.value]
+  const allImage = [...imagesFiles.value, filePresentation.value];
 
   const formData = new FormData();
   allImage.forEach((file) => {
     formData.append('images', file as File);
   });
 
-  formData.append('name', name.value)
-  formData.append('description', description.value)
-  formData.append('realisationTime', realisationTime.value.toString())
-  formData.append('presentationImage', presentationImage.value)
+  formData.append('name', name.value);
+  formData.append('description', description.value);
+  formData.append('realisationTime', realisationTime.value.toString());
+  formData.append('presentationImage', presentationImage.value);
 
-  if(video.value){
-    formData.append('video', video.value)
+  if (video.value) {
+    formData.append('video', video.value);
   }
 
 
