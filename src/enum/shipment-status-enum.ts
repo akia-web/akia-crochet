f2cb08a1c3835d48faeec53773521ea72c3f8d3e
akia <ch.royer15@gmail.com>
2026-06-04 18:32:03 +0200

    add github actions deploy prod

--- src/enum/shipment-status-enum.ts
+++ src/enum/shipment-status-enum.ts
@@ -2,7 +2,7 @@ export enum ShipmentStatusEnum {
   ACCEPTED = 'ACCEPTED',
   ANNOUNCED = 'ANNOUNCED', //	Le colis est soit récupéré par le transporteur, soit déposé dans un point de proximité
   SHIPPED = 'SHIPPED', // expédié
-  IN_TRANSIT = 'INTRANSIT',// Le colis a été scanné par le transporteur et est en transit
+  IN_TRANSIT = 'IN_TRANSIT',// Le colis a été scanné par le transporteur et est en transit
   OUT_FOR_DELIVERY = 'OUT_FOR_DELIVERY', //	Le colis est en cours de livraison
   FAILED_ATTEMPT = 'FAILED_ATTEMPT', //Quelque chose a empêché la livraison du colis
   REACHED_DELIVERY_PICKUP_POINT = 'REACHED_DELIVERY_PICKUP_POINT', //Le colis est disponible pour être récupéré dans un point de proximité
